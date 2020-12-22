08a59c08dc85d84d3dcb258d9b1062cb6685bea1
Marcos Garcia de Lima <marckfree@gmail.com>
2020-12-22 10:58:10 -0300

    Add GA

--- skvggor-site/pages/_document.js
+++ skvggor-site/pages/_document.js
@@ -4,7 +4,12 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-M7DD801H4P" />
+          <script dangerouslySetInnerHTML={{
+            __html: `window.dataLayer=window.dataLayer || []; function gtag(){dataLayer.push(arguments);}gtag('js', new Date()); gtag('config', 'G-M7DD801H4P');`
+          }}/>
+        </Head>
         <body>
           <Main />
           <NextScript />
