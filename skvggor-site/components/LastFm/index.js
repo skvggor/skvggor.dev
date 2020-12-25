e61fbbfcbf3c7db901f99433855cec3d672df626
Marcos Garcia de Lima <marckfree@gmail.com>
2020-12-25 15:29:26 -0300

    Refactor

--- skvggor-site/components/LastFm/index.js
+++ skvggor-site/components/LastFm/index.js
@@ -3,18 +3,6 @@ import Link from '../Link'
 import styles from './index.module.sass'
 
 function LastFm(props) {
-  const template = obj => (
-    <div className={styles.content}>
-      <div className={styles['container-image']}>
-        {obj.image}
-      </div>
-      <div className={styles['container-text']}>
-        {obj.music}
-        {obj.artist}
-      </div>
-    </div>
-  )
-
   const music = props.data.lastFmData.recenttracks.track.map((track, index) => {
     if (index === 0) {
       const text  = '@attr' in track ?
@@ -24,17 +12,26 @@ function LastFm(props) {
       const src = track.image[3]['#text'] ||
                   '/music-no-image.webp'
 
-      return template({
-        image: <img
-                className={styles.image}
-                src={src}
-                alt={`${text}'${track.name}' by '${track.artist['#text']}'`}
-                width="70"
-                height="70"
-              />,
-        music: <span className={styles.line}>{text}'<Link link={track.url} text={track.name} />'</span>,
-        artist: <span className={styles.line}>by '<Link link={`https://www.last.fm/music/${track.artist['#text'].replace(/\s/g, '+')}`} text={track.artist['#text']} />'</span>
-      })
+      return (
+        <div
+          key={index}
+          className={styles.content}
+        >
+          <div className={styles['container-image']}>
+            <img
+              className={styles.image}
+              src={src}
+              alt={`${text}'${track.name}' by '${track.artist['#text']}'`}
+              width="70"
+              height="70"
+            />
+          </div>
+          <div className={styles['container-text']}>
+            <span className={styles.line}>{text}'<Link link={track.url} text={track.name} />'</span>
+            <span className={styles.line}>by '<Link link={`https://www.last.fm/music/${track.artist['#text'].replace(/\s/g, '+')}`} text={track.artist['#text']} />'</span>
+          </div>
+        </div>
+      )
     }
   })
 
