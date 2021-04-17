3ec51c0c4df68360d6f6d8b61909abaca9e2a503
Marcos Garcia de Lima <marckfree@gmail.com>
2021-04-17 14:01:05 -0300

    Add audio visualizer

--- skvggor-site/components/LastFm/index.js
+++ skvggor-site/components/LastFm/index.js
@@ -1,13 +1,70 @@
+import React, { useEffect, useState } from 'react'
+
 import Link from '../Link'
 
 import styles from './index.module.sass'
 
 function LastFm(props) {
+  const [heightVisualizer, setHeightVisualizer] = useState([ 5, 5, 5, 5, 5, 5, 5, ])
+  const MAX_HEIGHT_BAR = 60
+  let showVisualizer = false
+
+  useEffect(() => {
+    if (showVisualizer) {
+      const interval = setInterval(() => {
+        setHeightVisualizer([
+          Math.floor(Math.random() * MAX_HEIGHT_BAR),
+          Math.floor(Math.random() * MAX_HEIGHT_BAR),
+          Math.floor(Math.random() * MAX_HEIGHT_BAR),
+          Math.floor(Math.random() * MAX_HEIGHT_BAR),
+          Math.floor(Math.random() * MAX_HEIGHT_BAR),
+          Math.floor(Math.random() * MAX_HEIGHT_BAR),
+          Math.floor(Math.random() * MAX_HEIGHT_BAR),
+        ])
+      }, 100)
+
+      return () => {
+        clearInterval(interval)
+      }
+    }
+  })
+
+  const renderBars = () => (
+    heightVisualizer.map((barHeight, index) => {
+      let backgroundColor
+
+      if (barHeight > 0) {
+        backgroundColor = '#ffffff'
+      }
+
+      if (barHeight > 20) {
+        backgroundColor = '#41b883'
+      }
+
+      if (barHeight > 40) {
+        backgroundColor = '#61dafb'
+      }
+
+      const span = <span
+        key={index}
+        style={{ backgroundColor: backgroundColor, height: barHeight }}
+      />
+
+      return span
+    })
+  )
+
   const music = props.data.lastFmData.recenttracks.track.map((track, index) => {
     if (index === 0) {
-      const text  = '@attr' in track ?
-                    'Now listening to ' :
-                    'Listened to '
+      let text
+
+      if ('@attr' in track) {
+        text = 'Now listening to '
+        showVisualizer = true
+      } else {
+        text = 'Listened to '
+        showVisualizer = false
+      }
 
       const src = track.image[3]['#text'] ||
                   '/music-no-image.webp'
@@ -18,6 +75,9 @@ function LastFm(props) {
           className={styles.content}
         >
           <div className={styles['container-image']}>
+            <span className={styles['visualizer']}>
+              {showVisualizer && renderBars()}
+            </span>
             <img
               className={styles.image}
               src={src}
